a5f70a6569d5b7f264f2494b752835ab95a95f38
Slovík, Michal <michal.slovik@exxeta.sk>
2020-03-18 18:21:34 +0100

    Setup repository with junit5 and first test

---
+++ src/main/java/lists/P01/P01.java
@@ -0,0 +1,10 @@
+package lists.P01;
+
+import java.util.List;
+
+public class P01 {
+
+    public static Object last(List<String> asList) {
+        return null;
+    }
+}
---
+++ src/test/java/lists/P01/p01Test.java
@@ -0,0 +1,17 @@
+package lists.P01;
+
+import org.junit.jupiter.api.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class p01Test {
+
+    @Test
+    public void shouldFindLastElementFromAListOfAlphabets() throws Exception {
+        assertThat(P01.last(asList("a", "b", "c", "d")), is(equalTo("d")));
+    }
+}
