85c8c3a1eba626826271f47bb3b20c2c0ba09f3c
Slovík, Michal <michal.slovik@exxeta.sk>
2020-03-19 13:31:05 +0100

    Cleaning class p02

--- src/main/java/lists/P02.java
+++ src/main/java/lists/P02.java
@@ -1,21 +1,12 @@
-/*
- * Classes with list manipulation.
- * <p>
- * These classes contain public static method with List methods.
- *
- * @author mishcos
- * @version 0.1
- * @since 0.1
- */
 package lists;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
- * Class for returning the secondLast item from list
+ * Class for returning the secondLast item from list.
  */
-public class P02 {
+public final class P02 {
 
     private P02() {
     }
@@ -24,9 +15,10 @@ public class P02 {
      * @param numbers input list of items
      * @return second last item
      */
-    public static Object secondLast(List<Integer> numbers) {
+    public static Object secondLast(final List<Integer> numbers) {
         if (numbers.isEmpty() || numbers.size() == 1) {
-            throw new NoSuchElementException("Empty list or only one element presented in the list");
+            throw new NoSuchElementException("Empty list or only "
+                    + "one element presented in the list");
         }
         return numbers.get(numbers.size() - 2);
     }
