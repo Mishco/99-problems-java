69435bab6de5e342750d3499696a6b0a8bd77a67
Michal Slovik <mishcos@gmail.com>
2020-03-20 13:41:16 +0100

    Update P04.java method

--- src/main/java/lists/P04.java
+++ src/main/java/lists/P04.java
@@ -2,11 +2,21 @@ package lists;
 
 import java.util.List;
 
+/**
+ * Class for getting length of the list
+ */
 public final class P04 {
 
-    private P04() { }
+    private P04() {
+    }
 
-    public static int length(final List<Integer> list) {
-        return 0;
+    /**
+     * Get length of the list.
+     *
+     * @param list list of item
+     * @return size of the list
+     */
+    public static <T> Integer length(final List<T> list) {
+        return list.size();
     }
 }
