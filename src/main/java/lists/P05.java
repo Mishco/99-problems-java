4f7f41b1d19e947d62333a1933b8b841212a12bb
Slovík, Michal <michal.slovik@exxeta.sk>
2020-03-21 15:39:44 +0100

    Reverse of items in list using Collections

--- src/main/java/lists/P05.java
+++ src/main/java/lists/P05.java
@@ -1,6 +1,6 @@
 package lists;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -19,10 +19,7 @@ public class P05 {
      * @return reversed list
      */
     public static <T> List<T> reverse(List<T> numbers) {
-        List<T> result = new ArrayList<>();
-        for (int i = numbers.size() - 1; i >= 0; i--) {
-            result.add(numbers.get(i));
-        }
-        return result;
+        Collections.reverse(numbers);
+        return numbers;
     }
 }
