53c1a4dc78d3d88cef0dd3e01cada8953878bb73
Michal Slovik <mishcos@gmail.com>
2020-03-23 17:34:06 +0100

    Solved isPalindrome method

--- src/main/java/lists/P06.java
+++ src/main/java/lists/P06.java
@@ -1,10 +1,36 @@
 package lists;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
-public class P06 {
+/**
+ * Class for String manipulation.
+ * </p>
+ * Find the palindrome
+ */
+public final class P06 {
 
-    public static <T>boolean isPalindrome(List<T> list) {
-        return false;
+    private P06() {
+    }
+
+
+    /**
+     * Find if the list of items is palindrome or not.
+     *
+     * @param list list of items
+     * @param <T>  type of item in input list
+     * @return true if palindrome, false it not
+     */
+    public static <T> boolean isPalindrome(List<T> list) {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException("List is empty");
+        }
+        for (int i = 0, j = list.size() - 1; i < list.size() / 2; i++, j--) {
+            if (list.get(i) != list.get(j)) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
---
+++ src/test/java/lists/P06Test.java
@@ -0,0 +1,49 @@
+package lists;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class P06Test {
+
+
+    @ParameterizedTest
+    @MethodSource("generateValidPalindromeStream")
+    public void testValidPalindrome(List<String> list) {
+        assertTrue(P06.isPalindrome(list));
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateInvalidPalindromes")
+    public void testInvalidPalindrome(List<String> list) {
+        assertFalse(P06.isPalindrome(list));
+    }
+
+    private static Stream<Arguments> generateValidPalindromeStream() {
+        List<Arguments> listOfArguments = new LinkedList<>();
+        listOfArguments.add(Arguments.of(Arrays.asList("x", "a", "m", "a", "x")));
+        listOfArguments.add(Arguments.of(Arrays.asList("b", "a", "b")));
+        listOfArguments.add(Arguments.of(Arrays.asList("b", "a", "a", "b")));
+        listOfArguments.add(Arguments.of(Arrays.asList("a", "a", "a", "a", "a")));
+        listOfArguments.add(Arguments.of(Arrays.asList("Sator", "Arepo", "Tenet", "Opera", "Rotas")));
+        return listOfArguments.stream();
+    }
+
+    private static Stream<Arguments> generateInvalidPalindromes() {
+        List<Arguments> listOfArguments = new LinkedList<>();
+        listOfArguments.add(Arguments.of(Arrays.asList("x", "m", "a", "x")));
+        listOfArguments.add(Arguments.of(Arrays.asList("a", "b")));
+        listOfArguments.add(Arguments.of(Arrays.asList("a", "a", "b")));
+        listOfArguments.add(Arguments.of(Arrays.asList("a", "y", "a", "a", "a")));
+        listOfArguments.add(Arguments.of(Arrays.asList("Sator", "Tenet", "Opera", "Rotas")));
+        return listOfArguments.stream();
+    }
+}
