2e41be0ba75f9ad7b12ffcaa0c886bed7dcc7574
Michal Slovik <mishcos@gmail.com>
2020-03-24 20:04:16 +0100

    Solve problem 08 with compress of lists

--- src/main/java/lists/P08.java
+++ src/main/java/lists/P08.java
@@ -1,6 +1,9 @@
 package lists;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 
 public final class P08 {
@@ -9,6 +12,23 @@ public final class P08 {
     }
 
     public static <T> List<T> compress(List<T> inputList) {
-        return inputList;
+        Objects.requireNonNull(inputList, "List must be not null");
+
+        List<T> result = new ArrayList<>();
+        if (inputList.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        T first = inputList.get(0);
+        result.add(first);
+
+        for (T act : inputList) {
+            if (!act.equals(first)) {
+                first = act;
+                result.add(act);
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/lists/P08Tests.java
+++ src/test/java/lists/P08Tests.java
@@ -1,5 +1,6 @@
 package lists;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -23,5 +24,10 @@ public class P08Tests {
         List<String> compressedList = P08.compress(asList("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"));
         assertThat(compressedList, hasSize(6));
         assertThat(compressedList, contains("a", "b", "c", "a", "d", "e"));
+    }
+
+    @Test
+    public void shouldNotBeNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> P08.compress(null));
     }
 }
