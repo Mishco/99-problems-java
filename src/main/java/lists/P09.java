62df25140706b28377284c05c4a5d92ec7361cc7
Michal Slovik <mishcos@gmail.com>
2020-03-25 10:14:52 +0100

    Prepare for P09 consecutive duplicates of list elements into sublists.

---
+++ src/main/java/lists/P09.java
@@ -0,0 +1,13 @@
+package lists;
+
+import java.util.List;
+
+public final class P09 {
+
+    private P09() {
+    }
+
+    public static List<List<String>> pack(List<String> asList) {
+        return null;
+    }
+}
---
+++ src/test/java/lists/P09Tests.java
@@ -0,0 +1,33 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+public class P09Tests {
+    @Test
+    public void shouldReturnAListWithTwoListElementsWhenAListWithTwoUniqueElementsIsPassed() throws Exception {
+        List<List<String>> packedList = P09.pack(Arrays.asList("a", "b"));
+        assertThat(packedList, hasSize(2));
+        assertThat(packedList.get(0), contains("a"));
+        assertThat(packedList.get(1), contains("b"));
+    }
+
+    @Test
+    public void shouldPackConsecutiveDuplicatesInTheirOwnLists() throws Exception {
+        List<List<String>> packedList = P09.pack(Arrays.asList("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"));
+        assertThat(packedList, hasSize(6));
+        assertThat(packedList.get(0), contains("a", "a", "a", "a"));
+        assertThat(packedList.get(1), contains("b"));
+        assertThat(packedList.get(2), contains("c", "c"));
+        assertThat(packedList.get(3), contains("a", "a"));
+        assertThat(packedList.get(4), contains("d"));
+        assertThat(packedList.get(5), contains("e", "e", "e", "e"));
+    }
+
+}
