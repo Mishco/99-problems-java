04b7a49179ceb4e0e53563119b53bae27e320d52
Michal Slovik <mishcos@gmail.com>
2020-03-26 19:49:21 +0100

    Solve the P10 with previous problem

--- src/main/java/lists/P10.java
+++ src/main/java/lists/P10.java
@@ -1,16 +1,42 @@
 package lists;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Get encode number of repeated elements.
+ * <p>
+ * Example:
+ * ?- encode([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+ * X = [[4,a],[1,b],[2,c],[2,a],[1,d],[4,e]]
+ */
 public class P10 {
 
     private P10() {
     }
 
+    /**
+     * Use previous P09 to get packed list and only get size of this sublist
+     *
+     * @param inputList list of repeated elements
+     * @param <T>       type of item
+     * @return List of SimpleEntry[count, Element]
+     */
+    public static <T> List<SimpleEntry<Integer, T>> encode(List<T> inputList) {
+        List<SimpleEntry<Integer, T>> simpleEntries = new ArrayList<>();
+        Objects.requireNonNull(inputList, "List must be not null");
 
-    public static <T> List<AbstractMap.SimpleEntry<Integer, T>> encode(List<T> inputList) {
-        return new ArrayList<>();
+        if (inputList.isEmpty()) {
+            return simpleEntries;
+        }
+
+        return P09.pack(inputList)
+                .stream()
+                .map(subList -> new SimpleEntry<>(subList.size(), subList.get(0)))
+                .collect(toList());
     }
 }
