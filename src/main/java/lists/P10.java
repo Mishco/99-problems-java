d5410bb15c1597c0cee3c8467bac2053d28422fd
Michal Slovik <mishcos@gmail.com>
2020-03-26 19:31:28 +0100

    Prepare for P10

---
+++ src/main/java/lists/P10.java
@@ -0,0 +1,16 @@
+package lists;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P10 {
+
+    private P10() {
+    }
+
+
+    public static <T> List<AbstractMap.SimpleEntry<Integer, T>> encode(List<T> inputList) {
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/lists/P10Tests.java
@@ -0,0 +1,30 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class P10Tests {
+
+
+    @Test
+    public void shouldEncodeAList()  {
+        List<AbstractMap.SimpleEntry<Integer, String>> encodedList = P10.encode(Arrays.asList("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"));
+        assertThat(encodedList, hasSize(6));
+        assertThat(encodedList.get(0), is(equalTo(new AbstractMap.SimpleEntry<>(4, "a"))));
+        assertThat(encodedList.get(1), is(equalTo(new AbstractMap.SimpleEntry<>(1, "b"))));
+        assertThat(encodedList.get(2), is(equalTo(new AbstractMap.SimpleEntry<>(2, "c"))));
+        assertThat(encodedList.get(3), is(equalTo(new AbstractMap.SimpleEntry<>(2, "a"))));
+        assertThat(encodedList.get(4), is(equalTo(new AbstractMap.SimpleEntry<>(1, "d"))));
+        assertThat(encodedList.get(5), is(equalTo(new AbstractMap.SimpleEntry<>(4, "e"))));
+    }
+
+
+}
