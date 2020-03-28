8cc8542ac0d7075c6c5f2ea0893f87f5192ea231
Michal Slovik <mishcos@gmail.com>
2020-03-28 13:08:46 +0100

    Preparation for P11

---
+++ src/main/java/lists/P11.java
@@ -0,0 +1,47 @@
+package lists;
+
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Modify the result of problem 1.10 in such a way
+ * that if an element has no duplicates it is simply copied into the result list.
+ * Only elements with duplicates are transferred as [N,E] terms.
+ * <p/>
+ * <pre>
+ * Example:
+ * ?- encode_modified([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+ * X = [[4,a],b,[2,c],[2,a],d,[4,e]]
+ * </pre>
+ */
+public class P11 {
+
+    private P11() {
+    }
+
+    /**
+     * Encode modified
+     *
+     * @param list input list of items
+     * @param <T>  type of item
+     * @return encoded list, only elements
+     */
+    public static <T> List<T> encode_modified(List<T> list) {
+
+        List<SimpleEntry<Integer, T>> simpleEntryList = P09.pack(list)
+                .stream()
+                .map(subList -> new SimpleEntry<>(subList.size(), subList.get(0)))
+                .collect(toList());
+
+        List<SimpleEntry<Integer, T>> onlyOnes = simpleEntryList
+                .stream()
+                .filter(item -> item.getValue().equals(Integer.valueOf(1)))
+                .collect(toList());
+
+        return (List<T>) onlyOnes;
+    }
+}
---
+++ src/test/java/lists/P11Tests.java
@@ -0,0 +1,29 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class P11Tests {
+
+    @Test
+    public void shouldEncodeAList() {
+        List<Object> encodedList = P11.encode_modified(Arrays.asList("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"));
+        assertThat(encodedList, hasSize(6));
+        assertThat(encodedList.get(0), is(equalTo(new SimpleEntry<>(4, "a"))));
+        assertThat(encodedList.get(1), is(equalTo("b")));
+        assertThat(encodedList.get(2), is(equalTo(new SimpleEntry<>(2, "c"))));
+        assertThat(encodedList.get(3), is(equalTo(new SimpleEntry<>(2, "a"))));
+        assertThat(encodedList.get(4), is(equalTo("d")));
+        assertThat(encodedList.get(5), is(equalTo(new SimpleEntry<>(4, "e"))));
+    }
+
+}
