9c4f8627c91cae2afe1959dea0bb93cd2a4994d6
Michal Slovik <mishcos@gmail.com>
2020-04-01 15:45:02 +0200

    Add javadoc for P12

--- src/main/java/lists/P12.java
+++ src/main/java/lists/P12.java
@@ -1,10 +1,25 @@
 package lists;
 
-import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Given a run-length code list generated as specified in problem P11.
+ * Construct its uncompressed version.
+ */
 public class P12 {
-    public static List<String> decode(List<Serializable> asList) {
+
+    private P12() {
+    }
+
+
+    /**
+     * Uncompressed list
+     *
+     * @param list input list of items
+     * @param <T>  type of item
+     * @return list of uncompressed list
+     */
+    public static <T> List<T> decode(List<T> list) {
         return null;
     }
 }
