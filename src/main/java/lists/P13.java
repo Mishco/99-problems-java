1a8796cd125d16abb5fcdcf95b4e4da853e85d42
Michal Slovik <mishcos@gmail.com>
2020-04-03 15:52:15 +0200

    Solve P13 with using previous solutions

--- src/main/java/lists/P13.java
+++ src/main/java/lists/P13.java
@@ -1,10 +1,19 @@
 package lists;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 /**
- *
+ * Implement the so-called run-length encoding data compression method directly.
+ * I.e. don't explicitly create the sublists containing the duplicates,
+ * as in problem 1.09, but only count them. As in problem 1.11,
+ * simplify the result list by replacing the singleton terms [1,X] by X.
+ * </p>
+ * Example:
+ * ?- encode_direct([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+ * X = [[4,a],b,[2,c],[2,a],d,[4,e]]
  */
 public class P13 {
 
@@ -12,10 +21,15 @@ public class P13 {
     }
 
     /**
-     * @param list
-     * @return
+     * Encode direct all items
+     *
+     * @param list list of items
+     * @return list of simple entries
      */
-    public static List<AbstractMap.SimpleEntry<Integer, String>> encode_direct(List<String> list) {
-        return null;
+    public static <T> List<SimpleEntry<Integer, T>> encode_direct(List<String> list) {
+        return P09.pack(list)
+                .stream()
+                .map(sublist -> new SimpleEntry<>(sublist.size(), (T) sublist.get(0)))
+                .collect(toList());
     }
 }
