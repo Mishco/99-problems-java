eb14493bf966b1dd3a365640531fa80229b190dc
Michal Slovik <mishcos@gmail.com>
2020-04-04 17:23:39 +0200

    Add javadoc for P15

--- src/main/java/lists/P15.java
+++ src/main/java/lists/P15.java
@@ -2,7 +2,22 @@ package lists;
 
 import java.util.List;
 
+/**
+ * Duplicate the elements of a list a given number of times.
+ */
 public class P15 {
+
+    private P15() {
+    }
+
+    /**
+     * Duplicate items in list.
+     *
+     * @param list input list of items.
+     * @param i    number of item to duplicate.
+     * @param <T>  type of item.
+     * @return duplicated list of items
+     */
     public static <T> List<T> duplicate(List<T> list, Integer i) {
 
         return null;
