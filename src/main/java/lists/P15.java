1ca05a947eea5cf3e2c157deaf2a3167016521a2
Michal Slovik <mishcos@gmail.com>
2020-04-04 17:17:39 +0200

    Prepare for P15 assignment.

---
+++ src/main/java/lists/P15.java
@@ -0,0 +1,10 @@
+package lists;
+
+import java.util.List;
+
+public class P15 {
+    public static <T> List<T> duplicate(List<T> list, Integer i) {
+
+        return null;
+    }
+}
---
+++ src/test/java/lists/P15Tests.java
@@ -0,0 +1,21 @@
+package lists;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+public class P15Tests {
+
+    @Test
+    public void shouldDuplicateElementsInAList() {
+        List<String> duplicates = P15.duplicate(Arrays.asList("a", "b", "c"), 3);
+        assertThat(duplicates, hasSize(9));
+        assertThat(duplicates, contains("a", "a", "a", "b", "b", "b", "c", "c", "c"));
+    }
+}
