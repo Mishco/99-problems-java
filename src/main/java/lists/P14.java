39151f5ea66cff7d7938bc0352f3142ad6ebbd64
Michal Slovik <mishcos@gmail.com>
2020-04-04 16:51:01 +0200

    Solve P14

--- src/main/java/lists/P14.java
+++ src/main/java/lists/P14.java
@@ -1,16 +1,24 @@
 package lists;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Duplicate the elements of a list
+ */
 public class P14 {
 
     /**
-     *
      * @param list list items
-     * @param <T> type of item
-     * @return
+     * @param <T>  type of item
+     * @return duplicated list of elements
      */
     public static <T> List<T> duplicate(List<T> list) {
-        return null;
+        List<T> newTList = new ArrayList<>();
+        for (T act : list) {
+            newTList.add(act);
+            newTList.add(act);
+        }
+        return newTList;
     }
 }
