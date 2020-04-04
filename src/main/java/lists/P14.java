87a618dd6c844d3c516ebda4ae6baa754a39864d
Michal Slovik <mishcos@gmail.com>
2020-04-04 16:32:45 +0200

    Preparation P14

---
+++ src/main/java/lists/P14.java
@@ -0,0 +1,16 @@
+package lists;
+
+import java.util.List;
+
+public class P14 {
+
+    /**
+     *
+     * @param list list items
+     * @param <T> type of item
+     * @return
+     */
+    public static <T> List<T> duplicate(List<T> list) {
+        return null;
+    }
+}
---
+++ src/test/java/lists/P14Tests.java
@@ -0,0 +1,21 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class P14Tests {
+
+    @Test
+    public void shouldDuplicateElementsInAList() {
+        List<String> duplicates = P14.duplicate(Arrays.asList("a", "b", "c", "d"));
+        assertThat(duplicates, hasSize(8));
+        assertThat(duplicates, contains("a", "a", "b", "b", "c", "c", "d", "d"));
+    }
+}
