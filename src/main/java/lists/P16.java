02358baa09fa76038b53038ce2c25f911f057232
Slovík, Michal <michal.slovik@exxeta.sk>
2020-04-06 17:54:04 +0200

    Prepare tests P16

---
+++ src/main/java/lists/P16.java
@@ -0,0 +1,16 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class P16 {
+
+    private P16() {
+    }
+
+    public static <T> List<T> dropEveryNth(List<T> asList, Integer i) {
+        return null;
+    }
+}
---
+++ src/test/java/lists/P16Tests.java
@@ -0,0 +1,35 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+public class P16Tests {
+
+    @Test
+    public void shouldDropEveryNthElement() {
+        List<String> result = P16.dropEveryNth(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"), 3);
+        assertThat(result, hasSize(8));
+        assertThat(result, contains("a", "b", "d", "e", "g", "h", "j", "k"));
+    }
+    
+    @Test
+    public void shouldReturnSameListWhenNIsLessThanListSize() {
+        List<String> result = P16.dropEveryNth(Arrays.asList("a", "b"), 3);
+        assertThat(result, hasSize(2));
+        assertThat(result, contains("a", "b"));
+    }
+    
+    @Test
+    public void shouldReturnSameListWhenNIsZero() {
+        List<String> result = P16.dropEveryNth(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"), 0);
+        assertThat(result, hasSize(11));
+        assertThat(result, contains("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"));
+    }
+}
