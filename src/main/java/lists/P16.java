9a7520eb0fe12b27406c60ac66a7325c1c0f46de
Michal Slovik <mishcos@gmail.com>
2020-04-07 22:11:19 +0200

    Update P16

--- src/main/java/lists/P16.java
+++ src/main/java/lists/P16.java
@@ -1,8 +1,12 @@
 package lists;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+/**
+ * Class for remove every n-th item from list.
+ */
 public final class P16 {
 
     private P16() {
@@ -12,20 +16,17 @@ public final class P16 {
      * Remove every i-th item from list
      *
      * @param list list of items
-     * @param nth    i-th integer value
+     * @param nth  i-th integer value
      * @param <T>  type of item
      * @return updated list
      */
-    public static <T> List<T> dropEveryNth(List<T> list, Integer nth) {
+    public static <T> List<T> dropEveryNth(final List<T> list, final Integer nth) {
         if (nth > list.size() || nth <= 0) {
             return list;
         }
-        List<T> updatedList = new ArrayList<>();
-        for (int j = 0; j < list.size(); j++) {
-            if ((j + 1) % nth != 0) {
-                updatedList.add(list.get(j));
-            }
-        }
-        return updatedList;
+        return IntStream.range(0, list.size())
+                .filter(j -> (j + 1) % nth != 0)
+                .mapToObj(list::get)
+                .collect(Collectors.toList());
     }
 }
