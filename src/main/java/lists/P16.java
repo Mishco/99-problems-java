6eccd336887b3797d1fecaba63fa281a98720f7e
Michal Slovik <mishcos@gmail.com>
2020-04-07 22:09:17 +0200

    First solution for P16

--- src/main/java/lists/P16.java
+++ src/main/java/lists/P16.java
@@ -1,16 +1,31 @@
 package lists;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public final class P16 {
 
     private P16() {
     }
 
-    public static <T> List<T> dropEveryNth(List<T> asList, Integer i) {
-        return null;
+    /**
+     * Remove every i-th item from list
+     *
+     * @param list list of items
+     * @param nth    i-th integer value
+     * @param <T>  type of item
+     * @return updated list
+     */
+    public static <T> List<T> dropEveryNth(List<T> list, Integer nth) {
+        if (nth > list.size() || nth <= 0) {
+            return list;
+        }
+        List<T> updatedList = new ArrayList<>();
+        for (int j = 0; j < list.size(); j++) {
+            if ((j + 1) % nth != 0) {
+                updatedList.add(list.get(j));
+            }
+        }
+        return updatedList;
     }
 }
