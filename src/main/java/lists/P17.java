6236645b013a9cfc75275510384cbc11358e00f8
Michal Slovik <mishcos@gmail.com>
2020-04-07 22:25:41 +0200

    Add prepare for P17

---
+++ src/main/java/lists/P17.java
@@ -0,0 +1,11 @@
+package lists;
+
+import java.util.List;
+import java.util.Map;
+
+public class P17 {
+    public static Map<Boolean, List<String>> split(List<String> list, int nth) {
+
+        return null;
+    }
+}
