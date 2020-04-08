dcdd6a9a4be315703151373c2112698b48f53fa9
Michal Slovik <mishcos@gmail.com>
2020-04-08 09:49:51 +0200

    First solution for P18

--- src/main/java/lists/P18.java
+++ src/main/java/lists/P18.java
@@ -1,9 +1,17 @@
 package lists;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class P18 {
-    public static <T>List<T> slice(List<T> list, int i, int i1) {
-        return null;
+
+
+    public static <T> List<T> slice(List<T> list, int from, int until) {
+        List<T> innerList = new ArrayList<>();
+
+        for (int i = from - 1; i < until; i++) {
+            innerList.add(list.get(i));
+        }
+        return innerList;
     }
 }
