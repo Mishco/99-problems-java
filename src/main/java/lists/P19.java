0da2f129daa7039309bf819d3090021f22f85a76
Michal Slovik <mishcos@gmail.com>
2020-04-11 16:30:38 +0200

    First tests return green

--- src/main/java/lists/P19.java
+++ src/main/java/lists/P19.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class P19 {
     public static <T> List<T> rotate(List<T> list, int i) {
+        if (i == 0) {
+            return list;
+        }
         return null;
     }
 }
