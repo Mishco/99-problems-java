abb5c59bd5b1bf949ea20487a826a125d4fff7ed
Michal Slovik <mishcos@gmail.com>
2020-04-13 13:07:37 +0200

    Prepare for P22

---
+++ src/main/java/lists/P22.java
@@ -0,0 +1,9 @@
+package lists;
+
+import java.util.List;
+
+public class P22 {
+    public static List<Integer> range(final int firstIdx, int secondIdx) {
+        return null;
+    }
+}
---
+++ src/test/java/lists/P22Tests.java
@@ -0,0 +1,21 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class P22Tests {
+
+    @Test
+    public void shouldCreateARangeBetween4And9() {
+        List<Integer> range = P22.range(4, 9);
+        assertThat(range, hasSize(6));
+        assertThat(range, contains(4, 5, 6, 7, 8, 9));
+    }
+
+}
