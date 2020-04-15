41ed29b08a29b060a6a2645b07ea190dba8e83f3
Michal Slovik <mishcos@gmail.com>
2020-04-15 22:38:49 +0200

    Update P25

--- src/main/java/lists/P25.java
+++ src/main/java/lists/P25.java
@@ -2,8 +2,25 @@ package lists;
 
 import java.util.List;
 
-public class P25 {
-    public static <T>List<T> randomPermutation(List<T> collect) {
+/**
+ * Generate a random permutation of the elements of a list.
+ * Example:
+ * ?- rnd_permu([a,b,c,d,e,f],L).
+ * L = [b,a,d,c,e,f]
+ */
+public final class P25 {
+
+    private P25() {
+    }
+
+    /**
+     * Permuted list of items.
+     *
+     * @param collect collect of items
+     * @param <T>     type of item
+     * @return list of permuted items
+     */
+    public static <T> List<T> randomPermutation(final List<T> collect) {
         return P23.randomSelect(collect, collect.size());
     }
 }
