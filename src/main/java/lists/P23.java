192273ac65327415ca1da36a14ac473e10dbb867
Michal Slovik <mishcos@gmail.com>
2020-04-15 21:14:31 +0200

    Solve problem with invalid index

--- src/main/java/lists/P23.java
+++ src/main/java/lists/P23.java
@@ -1,17 +1,23 @@
 package lists;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class P23 {
     public static <T> List<T> randomSelect(List<T> list, int count) {
-        Random random = new Random();
         List<T> resArr = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            Object[] objects = P20.removeAt(list, random.nextInt(list.size()));
+        for (int i = 1; i <= count; i++) {
+            Object[] objects = P20.removeAt(list, getRandomNumberInRange(list.size()));
             resArr.add((T) objects[1]);
         }
         return resArr;
     }
+
+    private static int getRandomNumberInRange(int max) {
+        Random r = new SecureRandom();
+        return r.nextInt((max - 1) + 1) + 1;
+    }
+
 }
