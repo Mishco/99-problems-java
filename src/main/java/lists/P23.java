aa98b0c86656c734cf7eb2c07ee21b8e5a455c6c
Michal Slovik <mishcos@gmail.com>
2020-04-15 19:50:42 +0200

    Prepare for P23

---
+++ src/main/java/lists/P23.java
@@ -0,0 +1,10 @@
+package lists;
+
+import java.util.List;
+
+public class P23 {
+    public static <T> List<T> randomSelect(List<T> list, int count) {
+        return null;
+
+    }
+}
---
+++ src/test/java/lists/P23Tests.java
@@ -0,0 +1,21 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class P23Tests {
+
+    @Test
+    public void shouldReturnAListOfThreeRandomSelectedElements() {
+        List<String> result = P23.randomSelect(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"), 3);
+        System.out.println(result);
+        assertThat(result, hasSize(3));
+    }
+
+}
