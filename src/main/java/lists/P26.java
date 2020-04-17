178a662afbea6f68f04c92235311940419a8d40e
Michal Slovik <mishcos@gmail.com>
2020-04-17 21:44:03 +0200

    First hint

--- src/main/java/lists/P26.java
+++ src/main/java/lists/P26.java
@@ -1,9 +1,13 @@
 package lists;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class P26 {
     public static <T> List<List<T>> combinations(List<T> input, int i) {
-        return null;
+        List<T> randomSelect = P23.randomSelect(input, i);
+        List<List<T>> res = new ArrayList<>();
+        res.add(P25.randomPermutation(randomSelect));
+        return res;
     }
 }
