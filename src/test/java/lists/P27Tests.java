1ad64eff4b90572c0b302433eb2dc5f3bb582d8d
Michal Slovik <mishcos@gmail.com>
2020-04-19 12:17:20 +0200

    Add javadoc

--- src/test/java/lists/P27Tests.java
+++ src/test/java/lists/P27Tests.java
@@ -15,14 +15,14 @@ import static org.hamcrest.Matchers.hasSize;
 public class P27Tests {
 
     @Test
-    public void shouldGroupIntoThreeGroupsOfSize_2_3_and_4() throws Exception {
+    public void shouldGroupIntoThreeGroupsOfSize_2_3_and_4() {
         List<String> input = Stream.of("aldo", "beat", "carla", "david", "evi", "flip", "gary", "hugo", "ida").collect(toList());
         List<List<List<String>>> groups = P27.group3(input);
         assertThat(groups, hasSize(1260));
     }
 
     @Test
-    public void shouldGroupIntoThreeGroupsOfSize_2_2_and_5() throws Exception {
+    public void shouldGroupIntoThreeGroupsOfSize_2_2_and_5() {
         List<String> input = Stream.of("aldo", "beat", "carla", "david", "evi", "flip", "gary", "hugo", "ida").collect(toList());
         List<List<List<String>>> groups = P27.group(input, Stream.of(2, 2, 5).collect(Collectors.toList()));
         assertThat(groups, hasSize(756));
