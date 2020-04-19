287adad5a6a261bc0151423e1a8ed590fa49280c
Michal Slovik <mishcos@gmail.com>
2020-04-19 20:51:31 +0200

    Repair P26

--- src/main/java/lists/P26.java
+++ src/main/java/lists/P26.java
@@ -1,10 +1,12 @@
 package lists;
 
-import com.google.common.math.BigIntegerMath;
-
-import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * In how many ways can a committee of 3 be chosen from a group of 12 people?
@@ -27,46 +29,57 @@ public final class P26 {
 
     public static <T> List<List<T>> combinations(final List<T> input,
                                                  final int i) {
-        List<T> randomSelect = P23.randomSelect(input, i);
-        List<List<T>> res = new ArrayList<>();
-        // how many items will be in result ??? - math formula for that
-        BigInteger binomial = BigIntegerMath.binomial(input.size(), i);
-        // but we cannot know if result is not repeating
-        while (res.size() != binomial.intValue()) {
-            List<T> permutation = P25.randomPermutation(randomSelect);
-            //if(!res.contains(permutation)) {
-            res.add(permutation);
-            //}
+//        List<T> randomSelect = P23.randomSelect(input, i);
+        if (i == 0 || input.isEmpty()) {
+            return Collections.emptyList();
         }
-        return res;
+        if (i == 1) {
+            return input.stream()
+                    .map(e -> Stream.of(e)
+                            .collect(toList()))
+                    .collect(toList());
+        }
+        Map<Boolean, List<T>> tempMap = P17.split(input, 1);
+        List<T> first = tempMap.get(true);
+        List<T> second = tempMap.get(false);
 
-//        Map<Boolean, List<T>> headAndTail = P17.split(input, 1);
-//        List<T> head = headAndTail.get(true);
-//        List<T> tail = headAndTail.get(false);
-//        List<List<T>> c1 = combinations(tail, (i - 1)).stream().map(e -> {
-//            List<T> l = new ArrayList<>();
-//            l.addAll(head);
-//            l.addAll(e);
-//            return l;
-//        }).collect(Collectors.toList());
-//        List<List<T>> c2 = combinations(tail, i);
-//        c1.addAll(c2);
-//        return c1;
+
+        List<List<T>> c1 = combinations(second, (i - 1))
+                .stream()
+                .map(e -> {
+                    List<T> l = new ArrayList<>();
+                    l.addAll(first);
+                    l.addAll(e);
+                    return l;
+                }).collect(toList());
+
+        List<List<T>> c2 = combinations(second, i);
+        c1.addAll(c2);
+        return c1;
+//        List<List<T>> res = new ArrayList<>();
+//        // how many items will be in result ??? - math formula for that
+//        BigInteger binomial = BigIntegerMath.binomial(input.size(), i);
+//        // but we cannot know if result is not repeating
+//        while (res.size() != binomial.intValue()) {
+//            List<T> permutation = P25.randomPermutation(randomSelect);
+//            //if(!res.contains(permutation)) {
+//            res.add(permutation);
+//            //}
+//        }
+//        return res;
 
     }
 
     public static <T> List<String> combinationUtil(List<String> arr, List<String> data, int start,
-                                                   int end, int index, int r)
-    {
+                                                   int end, int index, int r) {
 //        List<List<T>> res = new ArrayList<>();
 
 
         // Current combination is ready to be printed, print it
-        if (index == r)
-        {
-            for (int j=0; j<r; j++)
-                System.out.print(data.get(j)+" ");
-                //res
+        if (index == r) {
+            for (int j = 0; j < r; j++)
+                System.out.print(data.get(j) + " ");
+            //res
             System.out.println("");
             return data;
         }
@@ -75,10 +88,9 @@ public final class P26 {
         // "end-i+1 >= r-index" makes sure that including one element
         // at index will make a combination with remaining elements
         // at remaining positions
-        for (int i=start; i<=end && end-i+1 >= r-index; i++)
-        {
+        for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
             data.set(index, arr.get(i));
-            combinationUtil(arr, data, i+1, end, index+1, r);
+            combinationUtil(arr, data, i + 1, end, index + 1, r);
         }
         return data;
     }
