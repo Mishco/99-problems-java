c76e6167a12b7287f3b73e386d27eb04d888205b
Michal Slovik <mishcos@gmail.com>
2020-04-19 20:53:24 +0200

    Refactor P26

--- src/main/java/lists/P26.java
+++ src/main/java/lists/P26.java
@@ -29,15 +29,14 @@ public final class P26 {
 
     public static <T> List<List<T>> combinations(final List<T> input,
                                                  final int i) {
-//        List<T> randomSelect = P23.randomSelect(input, i);
-        if (i == 0 || input.isEmpty()) {
-            return Collections.emptyList();
-        }
         if (i == 1) {
             return input.stream()
                     .map(e -> Stream.of(e)
                             .collect(toList()))
                     .collect(toList());
+        }
+        if (i == 0 || input.isEmpty()) {
+            return Collections.emptyList();
         }
         Map<Boolean, List<T>> tempMap = P17.split(input, 1);
         List<T> first = tempMap.get(true);
@@ -52,46 +51,9 @@ public final class P26 {
                     l.addAll(e);
                     return l;
                 }).collect(toList());
-
-        List<List<T>> c2 = combinations(second, i);
-        c1.addAll(c2);
+        c1.addAll(combinations(second, i));
         return c1;
-//        List<List<T>> res = new ArrayList<>();
-//        // how many items will be in result ??? - math formula for that
-//        BigInteger binomial = BigIntegerMath.binomial(input.size(), i);
-//        // but we cannot know if result is not repeating
-//        while (res.size() != binomial.intValue()) {
-//            List<T> permutation = P25.randomPermutation(randomSelect);
-//            //if(!res.contains(permutation)) {
-//            res.add(permutation);
-//            //}
-//        }
-//        return res;
-
     }
 
-    public static <T> List<String> combinationUtil(List<String> arr, List<String> data, int start,
-                                                   int end, int index, int r) {
-//        List<List<T>> res = new ArrayList<>();
 
-
-        // Current combination is ready to be printed, print it
-        if (index == r) {
-            for (int j = 0; j < r; j++)
-                System.out.print(data.get(j) + " ");
-            //res
-            System.out.println("");
-            return data;
-        }
-
-        // replace index with all possible elements. The condition
-        // "end-i+1 >= r-index" makes sure that including one element
-        // at index will make a combination with remaining elements
-        // at remaining positions
-        for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
-            data.set(index, arr.get(i));
-            combinationUtil(arr, data, i + 1, end, index + 1, r);
-        }
-        return data;
-    }
 }
