1cd264d915f78b95f1ccdb936f540a3b74309fe9
Michal Slovik <mishcos@gmail.com>
2020-05-02 18:38:01 +0200

    Fix issue with 26 in test

--- src/test/java/lists/P26Tests.java
+++ src/test/java/lists/P26Tests.java
@@ -2,20 +2,27 @@ package lists;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class P26Tests {
     @Test
-    public void shouldFindAllCombinationsOfSize3FromAListWithSize6()  {
+    public void shouldFindAllCombinationsOfSize3FromAListWithSize6() {
         List<String> input = Stream.of("a", "b", "c", "d", "e", "f").collect(toList());
         List<List<String>> combinations = P26.combinations(input, 3);
         assertThat(combinations, hasSize(20));
+    }
+
+    @Test
+    public void shouldReturnEmptyList() {
+        List<List<Object>> input = P26.combinations(Collections.emptyList(), 0);
+        assertTrue(input.isEmpty());
     }
 }
