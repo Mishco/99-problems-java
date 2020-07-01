658a88e5d4d210db447a9654a95fe7a1a3deafe2
Michal Slovik <mishcos@gmail.com>
2020-07-01 21:39:52 +0200

    Prepare tests for P34

---
+++ src/main/java/arithmetic/P34.java
@@ -0,0 +1,19 @@
+package arithmetic;
+
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class P34 {
+
+    private P34() {
+    }
+
+    public static List<AbstractMap.SimpleEntry<Integer, List<Integer>>> goldbachList(IntStream rangeClosed) {
+        return null;
+    }
+
+    public static List<AbstractMap.SimpleEntry<Integer, List<Integer>>> goldbachList1(IntStream rangeClosed, int i) {
+        return null;
+    }
+}
---
+++ src/test/java/arithmetic/P34Test.java
@@ -0,0 +1,44 @@
+package arithmetic;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+class P34Test {
+
+    @Test
+    @Disabled
+    void shouldProduceAListOfGoldbachCompositions() {
+        List<AbstractMap.SimpleEntry<Integer, List<Integer>>> compositions = P34.goldbachList(IntStream.rangeClosed(9, 20));
+        assertThat(compositions, hasSize(6));
+        assertThat(compositions, hasItems(
+                new AbstractMap.SimpleEntry<>(10, Arrays.asList(3, 7)),
+                new AbstractMap.SimpleEntry<>(12, Arrays.asList(5, 7)),
+                new AbstractMap.SimpleEntry<>(14, Arrays.asList(3, 11)),
+                new AbstractMap.SimpleEntry<>(16, Arrays.asList(3, 13)),
+                new AbstractMap.SimpleEntry<>(18, Arrays.asList(5, 13)),
+                new AbstractMap.SimpleEntry<>(20, Arrays.asList(3, 17))
+        ));
+    }
+
+    @Test
+    @Disabled
+    void shouldProduceAListOfGoldbachCompositionsWhereBothPrimeNumbersAreGreaterThan50() {
+        List<AbstractMap.SimpleEntry<Integer, List<Integer>>> compositions = P34.goldbachList1(IntStream.rangeClosed(1, 2000), 50);
+        assertThat(compositions, hasSize(4));
+        assertThat(compositions, hasItems(
+                new AbstractMap.SimpleEntry<>(992, Arrays.asList(73, 919)),
+                new AbstractMap.SimpleEntry<>(1382, Arrays.asList(61, 1321)),
+                new AbstractMap.SimpleEntry<>(1856, Arrays.asList(67, 1789)),
+                new AbstractMap.SimpleEntry<>(1928, Arrays.asList(61, 1867))
+        ));
+    }
+}
