e8f326f56ea9700191fd674080329436e5ff1d29
Michal Slovik <mishcos@gmail.com>
2020-07-01 20:10:11 +0200

    Small refactor of code

--- src/test/java/arithmetic/P33Test.java
+++ src/test/java/arithmetic/P33Test.java
@@ -11,14 +11,14 @@ import static org.junit.Assert.assertThat;
 class P33Test {
 
     @Test
-    public void _8_isthesumof_3_and_5() {
+    void _8_isthesumof_3_and_5() {
         List<Integer> numbers = P33.goldbach(8);
         assertThat(numbers, hasSize(2));
         assertThat(numbers, hasItems(3, 5));
     }
 
     @Test
-    public void _28_isthesumof_5_and_23() {
+    void _28_isthesumof_5_and_23() {
         List<Integer> numbers = P33.goldbach(28);
         assertThat(numbers, hasSize(2));
         assertThat(numbers, hasItems(5, 23));
