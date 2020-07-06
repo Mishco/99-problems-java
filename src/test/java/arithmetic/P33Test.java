97abc9bc39b96e3cbdee8564191ac9eec1ba95ad
Michal Slovik <mishcos@gmail.com>
2020-07-06 21:19:11 +0200

    Mutliple check of invalid input parameters

--- src/test/java/arithmetic/P33Test.java
+++ src/test/java/arithmetic/P33Test.java
@@ -2,10 +2,12 @@ package arithmetic;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 class P33Test {
@@ -22,5 +24,16 @@ class P33Test {
         List<Integer> numbers = P33.goldbach(28);
         assertThat(numbers, hasSize(2));
         assertThat(numbers, hasItems(5, 23));
+    }
+
+    @Test
+    void testInvalidInputAndMakeEmptyList() {
+        final var goldbachList = P33.goldbach(1);
+        assertEquals(Collections.emptyList(), goldbachList);
+    }
+    @Test
+    void testZeroInputAndMakeEmptyList() {
+        final var goldbachList = P33.goldbach(0);
+        assertEquals(Collections.emptyList(), goldbachList);
     }
 }
