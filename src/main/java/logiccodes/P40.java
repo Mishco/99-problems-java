465c610939baa4f23f5617cae239721af01e22fa
Michal Slovik <mishcos@gmail.com>
2020-07-07 21:51:17 +0200

    Add P40

--- src/main/java/logiccodes/P40.java
+++ src/main/java/logiccodes/P40.java
@@ -1,13 +1,40 @@
 package logiccodes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiPredicate;
 
 public final class P40 {
 
+    /**
+     * Default values of table for input params.
+     */
+    static final List<Boolean> BOOLEANS = Arrays.asList(true, false);
+
     private P40() {
     }
 
+    /**
+     * A logical expression in two variables can then be written.
+     * in prefix notation,
+     * as in the following example: and(or(A,B),nand(A,B)).
+     *
+     * @param function input function
+     * @return String table
+     */
     public static String table(final BiPredicate<Boolean, Boolean> function) {
-        return null;
+        List<String> resultBuilder = new ArrayList<>();
+        resultBuilder.add("A          B          result");
+        for (boolean a : BOOLEANS) {
+            for (boolean b : BOOLEANS) {
+                resultBuilder.add(
+                        String.format("%-10s %-10s %s",
+                                a,
+                                b,
+                                function.test(a, b)));
+            }
+        }
+        return String.join("\n", resultBuilder);
     }
 }
--- src/test/java/logiccodes/P40Test.java
+++ src/test/java/logiccodes/P40Test.java
@@ -16,6 +16,26 @@ class P40Test {
         return a && b;
     }
 
+    private Boolean equ(final Boolean a, final Boolean b) {
+        return or(and(a, b), and(!(a), !(b)));
+    }
+
+    private Boolean xor(final Boolean a, final Boolean b) {
+        return !(equ(a, b));
+    }
+
+    private Boolean nand(final Boolean a, final Boolean b) {
+        return !(and(a, b));
+    }
+
+    private Boolean nor(final Boolean a, final Boolean b) {
+        return !(or(a, b));
+    }
+
+    private Boolean impl(final Boolean a, final Boolean b) {
+        return or(!a, b);
+    }
+
     @Test
     void shouldGenerateTruthTable() {
         String table = P40.table((a, b) -> and(a, or(a, b)));
@@ -28,5 +48,49 @@ class P40Test {
         assertThat(table, is(equalTo(result)));
     }
 
+    @Test
+    void shouldGenerateBasicANDTable() {
+        String table = P40.table(this::and);
+        String result = "A          B          result\n" +
+                "true       true       true\n" +
+                "true       false      false\n" +
+                "false      true       false\n" +
+                "false      false      false";
+
+        assertThat(table, is(equalTo(result)));
+    }
+
+    @Test
+    void shouldGenerateBasicORTable() {
+        String table = P40.table(this::or);
+        String result = "A          B          result\n" +
+                "true       true       true\n" +
+                "true       false      true\n" +
+                "false      true       true\n" +
+                "false      false      false";
+        assertThat(table, is(equalTo(result)));
+    }
+
+    @Test
+    void shouldGenerateBasicEQUTable() {
+        String table = P40.table(this::equ);
+        String result = "A          B          result\n" +
+                "true       true       true\n" +
+                "true       false      false\n" +
+                "false      true       false\n" +
+                "false      false      true";
+        assertThat(table, is(equalTo(result)));
+    }
+
+    @Test
+    void shouldGenerateBasicXORTable() {
+        String table = P40.table(this::xor);
+        String result = "A          B          result\n" +
+                "true       true       false\n" +
+                "true       false      true\n" +
+                "false      true       true\n" +
+                "false      false      false";
+        assertThat(table, is(equalTo(result)));
+    }
 
 }
