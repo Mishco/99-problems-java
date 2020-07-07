9e3aec48f1fcd48a5aa94ad63d2005bd5d71bb5e
Michal Slovik <mishcos@gmail.com>
2020-07-07 21:03:56 +0200

    Update and compare phi methods

---
+++ src/main/java/arithmetic/P39.java
@@ -0,0 +1,8 @@
+package arithmetic;
+
+public final class P39 {
+    private P39() {
+    }
+
+    // This class if for testing purpose
+}
---
+++ src/test/java/arithmetic/P39Test.java
@@ -0,0 +1,29 @@
+package arithmetic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class P39Test {
+
+    @Test
+    void shouldCalculatePhiOf10090UsingP39()  {
+        long p = P37.phi(10090);
+        assertThat(p, equalTo(4032L));
+    }
+
+    @Test
+    void shouldCalculatePhiOf10090UsingP40()  {
+        long p = P38.phi(10090);
+        assertThat(p, equalTo(4032L));
+    }
+
+    @Test
+    void shouldReturnTheSameResult() {
+        long p1 = P38.phi(10090);
+        long p2 = P37.phi(10090);
+        assertEquals(p1, p2);
+    }
+}
