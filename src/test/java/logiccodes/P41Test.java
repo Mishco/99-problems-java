541ee6684c1710b9217c2a68f24c0efd375f0127
Michal Slovik <mishcos@gmail.com>
2020-07-13 13:44:08 +0200

    Update and upgrade junit version

--- src/test/java/logiccodes/P41Test.java
+++ src/test/java/logiccodes/P41Test.java
@@ -2,10 +2,7 @@ package logiccodes;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 class P41Test {
 
@@ -19,7 +16,30 @@ class P41Test {
                 "false      true       true\n" +
                 "false      false      true";
 
-        assertThat(table, is(equalTo(result)));
+        assertEquals(table, result);
     }
 
+    @Test
+    void shouldGenerateBasicNORTable() {
+        String table = P40.table(P41::nor);
+        String result = "A          B          result\n" +
+                "true       true       false\n" +
+                "true       false      false\n" +
+                "false      true       false\n" +
+                "false      false      true";
+
+        assertEquals(table, result);
+    }
+
+    @Test
+    void shouldGenerateBasicIMPLTable() {
+        String table = P40.table(P41::nor);
+        String result = "A          B          result\n" +
+                "true       true       true\n" +
+                "true       false      false\n" +
+                "false      true       true\n" +
+                "false      false      true";
+
+        assertEquals(table, result);
+    }
 }
