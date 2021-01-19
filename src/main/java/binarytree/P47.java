e96d275cd0381ed0b7216e217f2ff82976325a92
Michal <mishcos@gmail.com>
2021-01-19 09:47:29 +0000

    Prepare for P47

---
+++ src/main/java/binarytree/P47.java
@@ -0,0 +1,9 @@
+package binarytree;
+
+
+public final class P47 {
+    
+    private P47() {
+    }
+
+}
---
+++ src/test/java/binarytree/P47Test.java
@@ -0,0 +1,11 @@
+package binarytree;
+
+import org.junit.jupiter.api.Test;
+
+import static binarytree.P45.isTree;
+import static org.junit.jupiter.api.Assertions.*;
+
+class P47Test {
+
+    
+}
