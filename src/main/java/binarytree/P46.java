bf12f6c148dcba99c015357b1ef5463ac4d73677
Michal <mishcos@gmail.com>
2021-01-19 09:33:57 +0000

    Update src/main/java/binarytree/P46.java

--- src/main/java/binarytree/P46.java
+++ src/main/java/binarytree/P46.java
@@ -12,7 +12,7 @@ public final class P46 {
 
     public static AVLTree createBalancedTree(AVLTree tree) {
         if (tree.isEmpty()) {
-            return new List<Integer>();
+            return new LinkedList<Integer>();
         }            
         List<Integer> inorder = tree.getPrintInorder(tree.root);
         return tree;
