b1532245ae11bdd705ed026db387dd2050aaff85
Slovík, Michal <michal.slovik@exxeta.sk>
2021-01-21 11:33:03 +0100

    Add failing tests

--- src/main/java/binarytree/P47.java
+++ src/main/java/binarytree/P47.java
@@ -2,8 +2,36 @@ package binarytree;
 
 
 public final class P47 {
-    
+
     private P47() {
+    }
+
+    //4.03 (**) Symmetric binary trees
+
+    /**
+     * Symmetric definition:
+     * * The two root nodes have the same value
+     * * The left subtree of one root node is a mirror reflection
+     * of the right subtree of the other root node
+     */
+    public static boolean isSymmetricTree(AVLTree root) {
+        if (root.isEmpty()) {
+            return true;
+        }
+        return isMirror(root.root.left, root.root.right);
+    }
+
+    private static boolean isMirror(AVLTree.BVSNode root1, AVLTree.BVSNode root2) {
+        if (root1.height == 0 && root2.height == 0) {
+            return true;
+        } else if (root1.height == 0 || root2.height == 0) {
+            return false;
+        } else {
+            // If you interest if content is also symmetric
+            // root1.data == root2.data &&
+            return isMirror(root1.left, root2.right) &&
+                    isMirror(root1.right, root2.left);
+        }
     }
 
 }
--- src/test/java/binarytree/P47Test.java
+++ src/test/java/binarytree/P47Test.java
@@ -7,5 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class P47Test {
 
-    
+    @Test
+    void testBasicTree() {
+        AVLTree tree = new AVLTree();
+        assertTrue(P47.isSymmetricTree(tree));
+    }
+
+    @Test
+    void testOneItemTree() {
+        AVLTree tree = new AVLTree();
+        tree.root = tree.insert(tree.root, 1);
+
+        assertTrue(P47.isSymmetricTree(tree));
+    }
 }
