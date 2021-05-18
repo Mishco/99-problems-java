681f78ecec18c7d49b80c9df8140b5183f6e262e
Michal <michal.slovik@tutanota.com>
2021-05-18 05:19:05 +0200

    prepare 52

---
+++ src/main/java/binarytree/P52.java
@@ -0,0 +1,12 @@
+package binarytree;
+
+public final class P52 {
+
+    private P52() {
+
+    }
+
+    public static Integer countLeavesOfBinaryTree() {
+        return 0;
+    }
+}
---
+++ src/test/java/binarytree/P52Test.java
@@ -0,0 +1,13 @@
+package binarytree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P52Test {
+
+    @Test
+    void testGenerateAndTestParadigm() {
+        assertEquals(0,P52.countLeavesOfBinaryTree());
+    }
+}
